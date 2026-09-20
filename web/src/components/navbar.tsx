ee0d8e59bbd5807474678299c4f882fe95240906
levisantosp <levi.pires2005@gmail.com>
2026-09-20 19:53:08 -0300

    feat: add user account dropdown menu to navbar

--- web/src/components/navbar.tsx
+++ web/src/components/navbar.tsx
@@ -1,8 +1,15 @@
 'use client'
 
-import { House } from 'lucide-react'
+import { CircleUser, House } from 'lucide-react'
 import Link from 'next/link'
 import {
+  Button,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
   NavigationMenu,
   NavigationMenuItem,
   NavigationMenuLink,
@@ -17,18 +24,53 @@ const navItems = [
   }
 ] as const
 
+const dropItems = {
+  account: [
+    {
+      label: 'Perfil',
+      href: '/perfil'
+    }
+  ]
+} as const
+
 export function NavBar() {
   return (
-    <NavigationMenu>
-      <NavigationMenuList>
-        {navItems.map((item) => (
-          <NavigationMenuItem key={item.href}>
-            <NavigationMenuLink render={<Link href='/' />}>
-              {item.label}
-            </NavigationMenuLink>
-          </NavigationMenuItem>
-        ))}
-      </NavigationMenuList>
-    </NavigationMenu>
+    <div className='flex w-full justify-between items-center'>
+      <NavigationMenu>
+        <NavigationMenuList>
+          {navItems.map((item) => (
+            <NavigationMenuItem key={item.href}>
+              <NavigationMenuLink render={<Link href='/' />}>
+                {item.label}
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+          ))}
+        </NavigationMenuList>
+      </NavigationMenu>
+
+      <DropdownMenu>
+        <DropdownMenuTrigger
+          render={
+            <Button variant='ghost'>
+              <CircleUser size={40} />
+            </Button>
+          }
+        />
+
+        <DropdownMenuContent>
+          <DropdownMenuGroup>
+            <DropdownMenuLabel>Conta</DropdownMenuLabel>
+
+            {dropItems.account.map((item) => (
+              <Link key={item.href} href={item.href}>
+                <DropdownMenuItem className='cursor-pointer'>
+                  {item.label}
+                </DropdownMenuItem>
+              </Link>
+            ))}
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
   )
 }
