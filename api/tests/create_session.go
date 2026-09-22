b1020a511c6dbb5b14943ad533d134ee4c02d638
levisantosp <levi.pires2005@gmail.com>
2026-09-22 19:07:27 -0300

    feat: add CreateAdminSession test helper

--- api/tests/create_session.go
+++ api/tests/create_session.go
@@ -36,3 +36,32 @@ func CreateSession(user *generated.User, t *testing.T) middlewares.Session {
 
 	return session
 }
+
+func CreateAdminSession(
+	user *generated.User,
+	t *testing.T,
+) middlewares.Session {
+	session := middlewares.Session{
+		ID:          "admin-test-session",
+		UserId:      strconv.FormatInt(user.ID, 10),
+		Username:    user.Username,
+		DisplayName: user.DisplayName,
+		IsAdmin:     user.IsAdmin,
+	}
+
+	sessionJson, err := json.Marshal(session)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err := redis.Client.Set(
+		t.Context(),
+		"session:"+session.ID,
+		sessionJson,
+		time.Minute,
+	).Err(); err != nil {
+		t.Fatal(err)
+	}
+
+	return session
+}
